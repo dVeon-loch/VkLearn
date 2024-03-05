81219d66bc282669743c4947640e1edcd2800c1b
dVeon-loch <39005321+dVeon-loch@users.noreply.github.com>
2024-03-05 12:05:49 +0200

    Create Vulkan Instance

--- VkRenderer.cpp
+++ VkRenderer.cpp
@@ -1,7 +1,7 @@
 #include "VkRenderer.h"
 
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
+#include<cstring>
+#include<iostream>
 
 void VkRenderer::InitWindow()
 {
@@ -17,7 +17,7 @@ void VkRenderer::InitWindow()
 
 void VkRenderer::InitVulkan()
 {
-	// TODO
+	CreateInstance();
 }
 
 void VkRenderer::Cleanup()
@@ -32,4 +32,57 @@ void VkRenderer::MainLoop()
 	while (!glfwWindowShouldClose(_window)) {
 		glfwPollEvents();
 	}
+}
+
+void VkRenderer::CreateInstance()
+{
+	// Optional info for the driver
+	VkApplicationInfo appInfo{}; // Curly braces very important here, they initialize all fields to 0/null/a defined value
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pApplicationName = "Hello Triangle";
+	appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.pEngineName = "No Engine";
+	appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.apiVersion = VK_API_VERSION_1_0; // TODO: Update version
+
+	// Mandatory info to create the instance
+	VkInstanceCreateInfo instanceCreateInfo{};
+	instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	instanceCreateInfo.pApplicationInfo = &appInfo;
+
+	std::vector<std::string> requiredExtensions = GetRequiredExtensions();
+
+	std::vector<const char*> extensionsCString;
+	extensionsCString.reserve(requiredExtensions.size());
+
+	for (const auto& extension : requiredExtensions)
+	{
+		extensionsCString.push_back(extension.c_str());
+	}
+
+
+	instanceCreateInfo.enabledExtensionCount = static_cast<uint32_t>(requiredExtensions.size());
+	instanceCreateInfo.ppEnabledExtensionNames = extensionsCString.data();
+	instanceCreateInfo.enabledLayerCount = 0; // TODO: Enable validation layers
+
+	VK_CHECK_RESULT(vkCreateInstance(&instanceCreateInfo, nullptr, &_instance), "create instance");
+}
+std::vector<std::string> VkRenderer::GetRequiredExtensions()
+{
+	std::vector<std::string> requiredExtensions;
+
+	/// GLFW EXTENSIONS
+	uint32_t glfwExtensionCount = 0;
+	const char** glfwExtensions;
+
+	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+
+	for (size_t i = 0; i < glfwExtensionCount; i++)
+	{
+		const std::string extension(glfwExtensions[i]);
+		requiredExtensions.push_back(extension);
+	}
+
+	return requiredExtensions;
 }
--- VkRenderer.h
+++ VkRenderer.h
@@ -1,4 +1,13 @@
 #pragma once
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include<vector>
+#include<string>
+#include <stdexcept>
+
+#include <vulkan/vk_enum_string_helper.h >
+
 class VkRenderer
 {
 private:
@@ -7,6 +16,8 @@ private:
     static constexpr uint32_t HEIGHT = 600;
 
     GLFWwindow* _window;
+
+    VkInstance _instance;
 
 public:
     void run() {
@@ -23,5 +34,19 @@ public:
 	void Cleanup();
 
     void MainLoop();
+
+private:
+    void CreateInstance();
+    std::vector <std::string> GetRequiredExtensions();
+
+    static void VK_CHECK_RESULT(VkResult result, std::string action)
+    {
+#ifdef _DEBUG 
+        if (result != VK_SUCCESS) {
+            // Using string_VkResult to convert the result code into its string equivalent
+            throw std::runtime_error("failed to " + action + "!. Error: " + string_VkResult(result) );
+        }
+#endif
+    }
 };
 
