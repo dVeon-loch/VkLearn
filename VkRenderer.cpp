3569450524436b81cd66a5293bc28211b1a0e240
dVeon-loch <39005321+dVeon-loch@users.noreply.github.com>
2024-09-10 15:39:55 +0200

    Attempt to fix texture

--- VkRenderer.cpp
+++ VkRenderer.cpp
@@ -1605,7 +1605,7 @@ void VkRenderer::LoadModel()
 
 			vertex.texCoord = {
 				attrib.texcoords[2 * index.texcoord_index + 0],
-				attrib.texcoords[2 * index.texcoord_index + 1]
+				1.0f - attrib.texcoords[2 * index.texcoord_index + 1]
 			};
 
 			vertex.colour = { 1.0f, 1.0f, 1.0f };
@@ -1624,7 +1624,7 @@ void VkRenderer::UpdateUniformBuffer(uint32_t currentImage)
 	float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
 
 	UniformBufferObject ubo{};
-	ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+	ubo.model = glm::rotate(glm::mat4(1.0f), time * 0.3f *  glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
 
 	ubo.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
 
